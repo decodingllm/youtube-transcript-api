54f172b0c9e0cfe9df177132ce16c0307da1f780
decodingllm <decodingllm@gmail.com>
2025-02-16 12:50:58 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+def extract_transcript_text(apiResponse):
+    transcript_text = []
+    if isinstance(apiResponse, dict):
+        for key, value in apiResponse.items():
+            if key == "text":
+                transcript_text.append(value)
+            else:
+                transcript_text.extend(extract_transcript_text(value))
+    elif isinstance(apiResponse, list):
+        for item in apiResponse:
+            transcript_text.extend(extract_transcript_text(item))
+    return transcript_text
+
+def get_transcript_with_params(video_id, api_key, rapidapi_host, platform):
+    import http.client
+    import urllib.parse
+
+    conn = http.client.HTTPSConnection(rapidapi_host)
+
+    headers = {
+        'x-rapidapi-key': api_key,
+        'x-rapidapi-host': rapidapi_host
+    }
+
+    params = {
+        'video_id': video_id,
+        'platform': platform
+    }
+
+    url = "/api/v1/get-transcript-v2?" + urllib.parse.urlencode(params)
+
+    conn.request("GET", url, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
